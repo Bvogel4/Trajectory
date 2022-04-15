19aee3e04f2b795b9a50a76524a3ffef7ece7232
Blake Vogel <bvogel4@masonlive.gmu.edu>
2022-04-15 15:43:56 -0400

    updated to full run values

--- trajectory_generator.py
+++ trajectory_generator.py
@@ -8,21 +8,21 @@ from particle_sim import trajectory
 
 
 def trajectory_generator(par=True):
-    L_shell = np.linspace(2, 10, 2)
-    # pitch = np.linspace(90, 10, 8)
-    pitch_angle = [90]
+    #start with quick ones in parrallel first 
+    L_shell = np.linspace(10, 2, 5)
+    pitch_angle = np.linspace(90, 15,6)
     mass = [constants.M_p, constants. M_e]
     charge = [-constants.C_e, constants.C_e]
     species = ['proton', 'electron']
-    # K = np.logspace(8, 2, 7)
-    Kinetic_energy = [1e8]
+    Kinetic_energy =  np.logspace(8, 3, 6)
     parameters = constants.parameters
+    parameters.update({'losscone':True})
 
     if par is False:
-        for a in range(0, 1):  # len(mass)):  # skip electron for now
-            for b in range(0, len(Kinetic_energy)):
-                for c in range(0, len(pitch_angle)):
-                    for d in range(0, len(L_shell)):
+        for a in range(len(mass)):
+            for b in range(len(Kinetic_energy)):
+                for c in range(len(pitch_angle)):
+                    for d in range(len(L_shell)):
                         parameters.update({'L_shell': L_shell[d], 'pitch_angle':
                             pitch_angle[c], 'Kinetic_energy': Kinetic_energy[b],
                             'mass': mass[a], 'charge': charge[a],
@@ -31,8 +31,6 @@ def trajectory_generator(par=True):
                         trajectory(parameters)
 
     # use joblib to speed up compute time
-    # carful when running this, I think if memory gets cached, it will break
-    # energy error plots
     if par is True:
         Parallel(n_jobs=-2, prefer='threads')(
             delayed(trajectory)({'L_shell': 2, 'pitch_angle': 90, 'mass': 
@@ -41,7 +39,7 @@ def trajectory_generator(par=True):
                     'longitude': 0, 'phase': 0,'method': 'boris', 'accuracy':
                     1e3, 'sampling': 36, 'loss_cone': False, 'show_timing': 
                     False})
-            for a in range(0, len(mass))
+            for a in range(len(mass))
             for b in range(len(Kinetic_energy))
             for c in range(len(pitch_angle))
             for d in range(len(L_shell)))
